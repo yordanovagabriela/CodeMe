f0018be8ed76b5657b6cf974330f71d74494c6be
yordanovagabriela <gabriela.georgieva.yordanova@gmail.com>
2015-02-22 21:27:01 +0200

    ceaser_cipher python program

---
+++ ceaser_cipher.py
@@ -0,0 +1,27 @@
+alphabet={0:'a',1:'b',2:'c',3:'d',
+	4:'e',5:'f',6:'g',7:'h',
+	8:'i',9:'j',10:'k',11:'l',
+	12:'m',13:'n',14:'o',15:'p',
+	16:'q',17:'r',18:'s',19:'t',
+	20:'u',21:'v',22:'w',23:'x',
+	24:'y',25:'z'	
+}
+
+def ceaser(string,n):
+	new_list=list(string)
+	last_list=[]
+	for i in new_list:
+		for m in alphabet:
+			if i.lower()==alphabet[m]:
+				if m==25:
+					if i==i.lower():
+						last_list.append(alphabet[n-1])
+					else:
+						last_list.append(alphabet[n-1].upper())
+				elif i==i.lower():
+					last_list.append(alphabet[n+m])
+				else:
+					last_list.append(alphabet[n+m].upper())
+	print ("".join(last_list))
+	
+ceaser("AaZ",7)
