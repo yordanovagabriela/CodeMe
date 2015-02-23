d84f679d5519a142cfdf4be825a3fa1ec3e70346
Gabriela Yordanova <gabriela.georgieva.yordanova@gmail.com>
2015-02-23 18:36:45 +0200

    Update filled_tetrahedron.py

--- filled_tetrahedron.py
+++ filled_tetrahedron.py
@@ -1,15 +1,13 @@
-def fill(my_list,litres):
+def tetrahedron_filled(tetrahedrons, water):
 	i=0
 	br=0
-	for numbers in my_list:
-		
-		volume=(my_list[i]*my_list[i]*my_list[i]*(2**0.5))/12000
-		if volume<=litres:
+	tetrahedrons.sort()
+	for edges in tetrahedrons:
+		volume=(tetrahedrons[i]*tetrahedrons[i]*tetrahedrons[i]*(2**0.5))/12000
+		if volume<=water:
 			br=br+1
-			litres=litres-volume
+			water=water-volume
 		i=i+1
 	print (br)
 
-list=[100,20,30]
-litress=10
-print (fill(list,litress))
+print (tetrahedron_filled([100000],10))
