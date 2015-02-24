afae30e94cb88e838e59d1c7e9ac9743d3280d94
Gabriela Yordanova <gabriela.georgieva.yordanova@gmail.com>
2015-02-24 16:27:18 +0200

    Update filled_tetrahedron.py

--- filled_tetrahedron.py
+++ filled_tetrahedron.py
@@ -1,12 +1,10 @@
 def tetrahedron_filled(tetrahedrons, water):
-	i=0
-	br=0
+	max_tetrahedrons=0
 	tetrahedrons.sort()
 	for edges in tetrahedrons:
-		volume=(tetrahedrons[i]*tetrahedrons[i]*tetrahedrons[i]*(2**0.5))/12000
+		volume=((edges**3)*(2**0.5))/12000
 		if volume<=water:
-			br=br+1
+			max_tetrahedrons=max_tetrahedrons+1
 			water=water-volume
-		i=i+1
-	return (br)
+	return (max_tetrahedrons)
 print (tetrahedron_filled([40,30,10],10))
